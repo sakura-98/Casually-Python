a2de3036ad28431496bad46400bfbb0c60d70881
SuSu <yzsu0211@gmail.com>
2018-08-11 16:32:17 +0800

    avoid friends' nickname include / \ : * ? " < > |

--- 微信伪集赞截图/gethead.py
+++ 微信伪集赞截图/gethead.py
@@ -7,9 +7,11 @@ except Exception:
     pass
 
 itchat.auto_login()
+index = 1
 for friend in itchat.get_friends():
     img = itchat.get_head_img(userName=friend["UserName"])
-    path = "./res/"+friend['NickName']+".png"
+    path = "./res/%d.png"%index
+    index += 1
     with open(path,'wb') as f:
         f.write(img)
 itchat.logout()
